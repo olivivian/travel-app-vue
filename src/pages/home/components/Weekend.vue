2915db36bb50383fc0de3cc6b3d90af43cce2f76
Axjy <frightingtime@163.com>
2019-04-14 15:27:46 +0800

    周末去哪

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+           <p class="item-title">{{item.title}}</p>
+           <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList:[{
+        id:'0001',
+        imgUrl:require('../../../assets/img/weekend/01.png'),
+        title:'大连圣亚海洋世界',
+        desc:'浪漫大连首站，浪漫的海洋主题乐园'
+      },
+        {
+          id:'0002',
+          imgUrl:require('../../../assets/img/weekend/01.png'),
+          title:'贵安欢乐世界',
+          desc:'浪漫贵安，浪漫的贵安欢乐世界'
+        }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
