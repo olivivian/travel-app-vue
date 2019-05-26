86e6f0b21a24536bb5acd037136665554736ad13
Axjy <frightingtime@163.com>
2019-05-26 17:46:17 +0800

    城市加ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,101 +1,84 @@
 <template>
-<div class="list" ref="wrapper">
-    <div>
-        <div class="area">
-            <div class="title border-topbottom">当前城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">{{this.currentCity}}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">热门城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div
+                            class="button-wrapper"
+                            v-for="item of hot"
+                            :key="item.id"
+                            @click="handleCityClick(item.name)"
+                    >
+                        <div class="button">{{item.name}}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-            </div>
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-            </div>
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
-                </div>
-                <div class="item border-bottom">
-                    阿拉伯
+            <div
+                    class="area"
+                    v-for="(item, key) of cities"
+                    :key="key"
+                    :ref="key"
+            >
+                <div class="title border-topbottom">{{key}}</div>
+                <div class="item-list">
+                    <div
+                            class="item border-bottom"
+                            v-for="innerItem of item"
+                            :key="innerItem.id"
+                            @click="handleCityClick(innerItem.name)"
+                    >
+                        {{innerItem.name}}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
     import Bscroll from 'better-scroll'
+    import { mapState, mapMutations } from 'vuex'
     export default {
-        name: "CityList",
+        name: 'CityList',
+        props: {
+            hot: Array,
+            cities: Object,
+            letter: String
+        },
+        computed: {
+            ...mapState({
+                currentCity: 'city'
+            })
+        },
+        methods: {
+            handleCityClick (city) {
+                this.changeCity(city)
+                this.$router.push('/')
+            },
+            ...mapMutations(['changeCity'])
+        },
+        watch: {
+            letter () {
+                if (this.letter) {
+                    const element = this.$refs[this.letter][0]
+                    this.scroll.scrollToElement(element)
+                }
+            }
+        },
         mounted () {
             this.scroll = new Bscroll(this.$refs.wrapper)
         }
     }
 </script>
-
 
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl'
